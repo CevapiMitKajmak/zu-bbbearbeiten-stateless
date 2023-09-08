486aa837ee9bb68ff7988a819fa0e790ef5bc4c8
CevapiMitKajmak <Crunch2404@hotmail.com>
2023-09-08 15:13:36 +0200

    fix

--- helper.py
+++ helper.py
@@ -1,18 +1,22 @@
+from datetime import date
 from dataclasses import dataclass
 
 items = []
 
+today_str = date.today().isoformat()
 
 @dataclass
 class Item:
     text: str
+    date: date
     isCompleted: bool = False
 
+def clear():
+    items.clear()
 
-def add(text):
+def add(text, date_str = today_str):
     text = text.replace('b', 'bbb').replace('B', 'Bbb')
-    items.append(Item(text))
-
+    items.append(Item(text, date.fromisoformat(date_str)))
 
 def get_all():
     return items
